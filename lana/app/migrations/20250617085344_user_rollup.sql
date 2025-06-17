dcb5113e75d6148bcbc52cb7960bef16ca25def5
counterweight <counterweightoperator@protonmail.com>
2025-06-17 11:02:45 +0200

    target table to rollup into

---
+++ lana/app/migrations/20250617085344_user_rollup.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+  id UUID PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  authentication_id UUID UNIQUE DEFAULT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
+  updated_at TIMESTAMPTZ NOT NULL,
+  deleted_at TIMESTAMPTZ NULL,
+  last_sequence INT NOT NULL,
+  last_event_id UUID NOT NULL
+); 
