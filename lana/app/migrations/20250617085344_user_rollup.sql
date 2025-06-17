8001531f2afd983bd26adfbd8193900ef2ebb04e
counterweight <counterweightoperator@protonmail.com>
2025-06-17 11:54:25 +0200

    function to rollup initialized event

--- lana/app/migrations/20250617085344_user_rollup.sql
+++ lana/app/migrations/20250617085344_user_rollup.sql
@@ -8,3 +8,27 @@ CREATE TABLE users (
     last_sequence INT NOT NULL
 );
 
+CREATE OR REPLACE FUNCTION fn_project_user_initialized (entity_id UUID, event_sequence INTEGER, recorded_at_timestamp TIMESTAMPTZ, event JSONB)
+    RETURNS VOID
+    SECURITY DEFINER
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    INSERT INTO users (
+        id,
+        created_at,
+        updated_at,
+        last_sequence,
+        email)
+    VALUES (
+        entity_id,
+        recorded_at_timestamp,
+        recorded_at_timestamp,
+        event_sequence,
+        event ->> 'email')
+ON CONFLICT
+    DO NOTHING;
+    RETURN;
+END;
+$$;
+
