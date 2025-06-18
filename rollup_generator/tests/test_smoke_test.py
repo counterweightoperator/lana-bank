976e89ae01503e015ca09a434bf9162d8c8b3ea6
counterweight <counterweightoperator@protonmail.com>
2025-06-18 15:32:07 +0200

    improve test name

--- rollup_generator/tests/test_smoke_test.py
+++ rollup_generator/tests/test_smoke_test.py
@@ -3,7 +3,7 @@ from click.testing import CliRunner
 from rollup_generator.cli import smoke_test
 
 
-def test_smoke_test():
+def test_smoke_test_outputs_message():
     runner = CliRunner()
     result = runner.invoke(smoke_test)
     assert result.exit_code == 0
