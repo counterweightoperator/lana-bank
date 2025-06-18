1a450cbcce9a1f855e07b655975d9a49f3bff3db
counterweight <counterweightoperator@protonmail.com>
2025-06-18 15:32:18 +0200

    a few tests to guide next steps

---
+++ rollup_generator/tests/test_parse_definition.py
@@ -0,0 +1,14 @@
+def test_parse_target_table_definition_creates_target_table():
+    assert 1 == 0
+
+
+def test_parse_event_definition_creates_event():
+    assert 1 == 0
+
+
+def test_parse_event_effect_definition_creates_event_effect():
+    assert 1 == 0
+
+
+def test_parse_full_entity_definition_creates_entity():
+    assert 1 == 0
