a4929724b66fd1b74cc7fe76631ff4b71c0fea39
counterweight <counterweightoperator@protonmail.com>
2025-06-19 09:44:52 +0200

    remove unused import

--- rollup_generator/rollup_generator/code_generation_service.py
+++ rollup_generator/rollup_generator/code_generation_service.py
@@ -2,8 +2,6 @@ from rollup_generator.definition_parser import (
     parse_json_into_target_table_definition,
 )
 
-from rollup_generator.definitions import TargetTableDefinition
-
 
 def generate_entity_migration_from_definition(json_content: dict) -> str:
     entity_name = json_content["entity_name"]
